6a6540f4e141c5d7061dcc84e71562418f1e5584
DouglasWWolf <douglaswolf@yahoo.com>
2023-02-20 10:05:46 -0800

    No commit message

--- sidewinder.gen/sources_1/bd/board/hdl/board_wrapper.v
+++ sidewinder.gen/sources_1/bd/board/hdl/board_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
-//Date        : Mon Feb 20 04:55:03 2023
+//Date        : Mon Feb 20 09:46:16 2023
 //Host        : simtool-5 running 64-bit Ubuntu 20.04.5 LTS
 //Command     : generate_target board_wrapper.bd
 //Design      : board_wrapper
